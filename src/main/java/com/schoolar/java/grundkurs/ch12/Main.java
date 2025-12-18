bf05124c180dda2567548dd811085780792a39d5
Ramazan Cinardere <ramazan.cinardere@ctream.de>
2025-12-18 14:57:08 +0100

    [ch 12]: annotations

--- src/main/java/com/schoolar/java/grundkurs/ch12/Main.java
+++ src/main/java/com/schoolar/java/grundkurs/ch12/Main.java
@@ -1,8 +1,52 @@
 package com.schoolar.java.grundkurs.ch12;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 public class Main {
 
-    public static void main( String[] args ) {
-        System.out.println("Welcome to chapter 12");
+    public static void main( String[] args ) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        var welcome = List.of( "Welcome", "to", "chapter 12" );
+        System.out.println( firstNumbers( welcome ) );
+
+        var numbers = List.of( 1, 2, 3, 4, 5, 6 );
+        System.out.println( firstNumbers( numbers ) );
+
+        var longNumbers = List.of( 1L, 2L, 3L, 4L );
+        System.out.println( firstNumbers( longNumbers ) );
+
+
+        System.out.println( isListEmpty( welcome ) );
+        System.out.println( isListEmpty( numbers ) );
+        System.out.println( isListEmpty( longNumbers ) );
+        System.out.println( isListEmpty( List.of() ) );
+
+
+        // repository
+//        var repo = new PersonRepository();
+        var person = new Person();
+
+//        repo.save( person );
+//        Person found = repo.findById( 123L);
+//        System.out.println(found);
+
+
+        // bitte nicht nachmachen!!!
+        var clazzPerson = Person.class;
+        var personCreatedByReflection = clazzPerson.getConstructor(  ).newInstance(  );
+        System.out.println(personCreatedByReflection);
+
+        // TODO: Aufgabe: Erstelle ein JPA Interface, über welches Person Entitäten in die DB gespeichert,
+        //  gelöscht, geladen werden können.
+
+    }
+
+    public static <T> boolean isListEmpty( List<T> input ) {
+        return input != null && input.isEmpty();
+    }
+
+    //
+    public static <T> T firstNumbers( List<T> input ) {
+        return input.getFirst();
     }
 }
---
+++ src/main/java/com/schoolar/java/grundkurs/ch12/Person.java
@@ -0,0 +1,11 @@
+package com.schoolar.java.grundkurs.ch12;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private Long id;
+    private String username;
+    private Integer age;
+}
